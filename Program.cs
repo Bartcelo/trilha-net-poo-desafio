8ab2b1fe2413728607d26384577ac066cdfd1279
marcelo <bart.celo@hotmail.com>
2025-08-12 13:35:01 -0300

    Gerando testes das implementações feitas para verificar o funcinamento

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia Smartphone");
+Nokia nokia = new("77 9595959595", "1100", "123456#123456", 4);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake game");
+
+
+
+
+Console.WriteLine("Iphone Smartphone");
+Iphone iphone = new("77 5959595959", "iphone", "123456#123456", 8);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Orkut");
