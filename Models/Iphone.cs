f04e6d0f96a3e10c149815ea3786ba0951ab45f0
marcelo <bart.celo@hotmail.com>
2025-08-12 13:19:35 -0300

    Inserindo o construtor com os argumentos solicitados pela classe Smartphone na classe Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,14 +3,13 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
-        public  Iphone(string numero) : base (numero)
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         { }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
-
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"O nome do Aplicativo Iphone é {nomeApp}");
-         }
+        }
     }
 }
--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -5,7 +5,7 @@ namespace DesafioPOO.Models
     {
 
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
-         { }
+        { }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
